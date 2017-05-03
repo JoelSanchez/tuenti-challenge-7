e09fc4c670ab08f6488da306e62d8633af9823fd
Joel Sánchez <webmaster@kazer.es>
2017-05-03 23:54:28 +0200

    Eliminar core y core2 en challenge9 para evitar confusiones

--- src/tuentichallenge/challenge9/core.clj
+++ src/tuentichallenge/challenge9/core.clj
@@ -1,110 +1,154 @@
 (ns tuentichallenge.challenge9.core
   (:require [clojure.data.priority-map :refer [priority-map]]))
 
-(defn insert [vec pos item] 
-    (apply merge (subvec vec 0 pos) item (subvec vec pos)))
+(defn evenize [n]
+  (if (odd? n) (dec n) n))
 
-(defn apply-vector [pos v]
-  (merge-with + pos v))
+(defn solve-case* [{:keys [s c d] :as state}]
+  (-> state
 
-(defn double-vector [v]
-  (into {} (map (fn [[k i]] [k (* i 2)]) v)))
+    ;; setters
+    (assoc :solution 0)
+    (assoc :line-length 0)
+    (assoc :used-ss 0)
+    (assoc :used-ds 0)
+    (assoc :used-cs 0)
 
-(defn vector-by-type [type]
-  (case type
-    :up {:x 0 :y -1}
-    :right {:x 1 :y 0}
-    :down {:x 0 :y 1}
-    :left {:x -1 :y 0}))
+    ((fn [i] (println i) i))
 
-(defn turn-vector-type [type orientation]
-  (case type
-    :up (if (= orientation :cw) :right :left)
-    :right (if (= orientation :cw) :down :up)
-    :down (if (= orientation :cw) :left :right)
-    :left (if (= orientation :cw) :up :down)))
+    ;; double reducer
+    ((fn [i]
+        (let [ds (evenize (:d i))]
+          (-> i
+            (update :solution + ds)
+            (update :line-length + (* ds 2))
+            (update :d - ds)
+            (update :used-ds + ds)))))
 
-(defn nxt-short [nxts {:keys [s c d pos v-type way] :as state}]
-  (if (zero? s) nxts (conj nxts (-> state (update :s dec) (update :pos apply-vector (vector-by-type v-type)) (assoc :piece :s)))))
+    ((fn [i] (println "double reducer" i) i))
 
-(defn nxt-double [nxts {:keys [s c d pos v-type way] :as state}]
-  (if (zero? d) nxts (conj nxts (-> state (update :d dec) (update :pos apply-vector (double-vector (vector-by-type v-type))) (assoc :piece :d)))))
+    ;; single reducer
+    ((fn [i]
+        (let [ss (- (evenize (:s i)) 2)]
+          (if (> ss 0)
+            (-> i
+              (update :solution + ss)
+              (update :line-length + (/ ss 2))
+              (update :s - ss)
+              (update :used-ss + ss))
+            i)
+          )))
 
-(defn nxt-curve [nxts {:keys [s c d pos v-type way] :as state} orientation]
-  (if (zero? c) nxts
-    (conj nxts (-> state (update :c dec)
-                   (assoc :v-type (turn-vector-type v-type orientation))
-                   (update :pos apply-vector (vector-by-type v-type))
-                   (assoc :piece :c)))))
+    ((fn [i] (println "single reducer" i) i))
 
-(defn filter-nxts [nxts way]
-  (reduce (fn [acc i] (if (.contains way (:pos i))
-                          acc
-                          (conj acc i))) [] nxts))
+    ;; curve reducer
+    ((fn [i]
+        (if (>= (:c i) 8)
+          (let [cs (* 4 (Math/floor (/ (evenize (- (:c i) 4)) 4)))]
+            (if (> cs 0)
+              (-> i
+                (update :solution + cs)
+                (update :c - cs)
+                (update :used-cs + cs))
+              i)
+            )
+          i)))
 
-(defn find-first [f coll]
-  (first (filter f coll)))
+    ((fn [i] (println "curve reducer" i) i))
 
-(defn nxt-ways [nxts]
-  ;(clojure.pprint/pprint nxts)
-  (map (fn [i] (update i :way conj (assoc (:pos i) :piece (:piece i)))) nxts))
+    ;; base connector
+    ((fn [i]
+      (-> i
+        (update :solution + 2)
+        (update :c - 2)
+        (update :used-cs + 2))))
 
-(defn impossible-solution [{:keys [s c d pos v-type way] :as state}]
-  (let [min-y (Math/abs (:y pos))
-        min-x (Math/abs (:x pos))
-        max-moves (+ s c (* d 2))
-        no-turns (and (not (zero? (:y pos))) (not (zero? (:x pos))) (zero? c))]
-    (or no-turns (> (dec (+ min-y min-x)) max-moves))))
+    ((fn [i] (println "base connector" i) i))
 
-(defn solved-solution-2 [{:keys [s c d pos v-type way] :as state}]
-  (let [new-state (-> state
-                      (update :pos apply-vector (vector-by-type v-type)))]
-    (if (= (:pos new-state) {:x 0 :y 0})
-      (count (:way new-state))
-      nil)))
+    ;; pair connector
+    ((fn [i] 
+        (-> i
+            (update :c - 2)
+            (update :solution + 2)
+            (update :used-cs + 2))))
 
-(defn solved-solution [{:keys [s c d pos v-type way] :as state}]
-  (if (and (= pos {:x 0 :y 1}) (= v-type :up))
-      (count (:way state))
-      nil))
+    ((fn [i] (println "pair connector" i) i))
 
-(defn nxt [{:keys [s c d pos v-type way] :as state}]
-  (if (impossible-solution state)
-    []
-    (if-let [solution (solved-solution state)]
-      (do (println "solved" {:solution solution}) (println) (println)
-            {:solution solution})
-      (let [nxts (-> [] (nxt-short state) (nxt-double state) (nxt-curve state :cw) (nxt-curve state :countercw) (filter-nxts way) (nxt-ways))]
-        ;(println "nxt debug") (clojure.pprint/pprint nxts)
-        nxts))))
+    ;; remaining singles and doubles
+    ((fn [i] 
+        (if (> (evenize (:s i)) 0)
+          (let [ds (:d i)
+                ss (evenize (:s i))]
+            (-> i (update :d - ds)
+                  (update :s - ss)
+                  (update :used-ss + ss)
+                  (update :used-ds + ds)
+                  (update :line-length + (/ (+ (* ds 2) ss) 2))
+                  (update :solution + (+ ds ss))))
+          i)))
 
-(defn solve-case* [{:keys [s c d pos v-type way all] :as state}]
-  (println "solve-case | state-n" state)
-  (let [n (nxt state)]
-     ;(println "nxt") (clojure.pprint/pprint n)
-    (if (or (get n :solution) (empty? n))
-      (if (empty? n) [] [(do (get n :solution))])
-      (doall (reduce (fn [acc i]
-                        ; (println "last-sol" (get (last acc) :solution) (last acc))
-                        (if (or (get (last acc) :solution) (= (last acc) all) (> (count acc) 50))
-                          acc
-                          (doall (concat acc (solve-case* i))))) [] n)))))
+    ((fn [i] (println "remaining singles and doubles" i) i))
+
+    ;; s upstairs
+    ((fn [i] 
+        (if (and (>= (:used-ss i) 2) (>= (:c i) 2))
+          (-> i
+              (update :c - 2)
+              (update :solution + 2)
+              (update :used-cs + 2)
+              (assoc :used-s-upstairs true))
+          i)))
+
+    ((fn [i] (println "s upstairs" i) i))
+
+    ;; d upstairs
+    ((fn [i] 
+        (if (and (= (mod (int (:used-cs i)) 4) 0)
+                 (>= (int (:used-cs i)) 8)
+                 (>= (:d i) 1)
+                 (> (:line-length i) 0)
+                 (not (:used-s-upstairs i)))
+          (-> i
+              (update :d - 1)
+              (update :solution + 1)
+              (update :used-ds + 1))
+          i)))
+
+    ((fn [i] (println "d upstairs" i) i))
+
+    
+
+    ;; curves over single or double
+    ((fn [i] 
+        (if (and (= (:used-ss i) 0)
+                 (= (:used-ds i) 0)
+                 (or (= (:d i) 1) (= (:s i) 1))
+                 (not (and (= (:d i) 1) (= (:s i) 1)))
+                 (> (:used-cs i) 0)
+                 (= (mod (int (:used-cs i)) 4) 0))
+          (-> i
+              (assoc :c 0)
+              (assoc :d 0)
+              (assoc :s 0)
+              (update :used-ds + (:d i))
+              (update :used-cs + (:c i))
+              (update :solution + (:s i) (:d i) (:c i)))
+          i)))
+
+    ((fn [i] (println "curves over single or double" i) i))))
+
 
 (defn solve-case [{:keys [s c d] :as state}]
-  (if (< c 2)
-    0
-    (let [state (-> state (assoc :v-type :right)
-                          (assoc :pos {:x 0 :y 0})
-                          (assoc :way [{:x 0 :y 0}])
-                          (update :c dec))]
-      (let [solutions (doall (solve-case* state))]
-        (if (> (count solutions) 0) (do (println "the solutions" solutions) (apply max solutions)) 0)))))
+  (cond
+    (< c 4) 0
+    (and (= d 0) (= s 0)) 4
+    :else (:solution (solve-case* state))))
 
 (defn process-case [idx case]
   (println "case: " idx case)
   (let [[s c d] (read-string (str "[" case "]"))
         solution (solve-case {:all (+ s c d) :s s :c c :d d})]
-    (str "Case #" (inc idx) ": " solution)
+    (str "Case #" (inc idx) ": " (int solution))
     ))
 
 (defn challenge-from-file [input output]
